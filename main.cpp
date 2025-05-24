2d0140cf9a2a803f16b75cd0d2b4ca496aa4c974
RBK14 <maciek.rybka2004@gmail.com>
2025-05-24 16:30:03 +0200

    Initial commit. Adding .gitignore

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <ostream>
+
+#include "algorithm/bellman_ford.h"
+#include "algorithm/dijkstra.h"
+#include "algorithm/kruskal.h"
+#include "algorithm/prim.h"
+#include "model/graph.h"
+#include "utility/graph_creator.h"
+
+int main() {
+    return 0;
+}
